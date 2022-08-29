4156c5694cd3feeefd866c68fd684a4385422551
Ramprasad A <ramprasad.a@razorpay.com>
2022-08-29 17:55:52 +0530

    Test fix 2

--- .github/scripts/read-podspec-version.sh
+++ .github/scripts/read-podspec-version.sh
@@ -4,7 +4,7 @@ file="razorpay-pod.podspec"     #the file where you keep your string name
 
 NEW_PODSPEC_VERSION=$1
 
-versionLine=$(grep 's.version ' $file)
+versionLine="$(grep 's.version ' $file)"
 version=${versionLine##* }
 
 echo $version
