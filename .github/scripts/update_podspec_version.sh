6b486782fe600497b7e6520731df5e7154ef24dc
Ramprasad A <ramprasad.a@razorpay.com>
2022-08-29 18:51:08 +0530

    Syntax fix 8

--- .github/scripts/update_podspec_version.sh
+++ .github/scripts/update_podspec_version.sh
@@ -4,12 +4,12 @@ file="razorpay-pod.podspec"     #the file where you keep your string name
 
 NEW_PODSPEC_VERSION=$1
 
-myUser="$(grep 's.version ' $file)"
+myUser="$(grep 's.version ' $file | sed 's/"//g')"
 lastVal=${myUser##* }
 
 sed -i -e "s/$lastVal/'$NEW_PODSPEC_VERSION'/g" $file
 
-echo "$lastVal"
+echo $lastVal
 
 # echo "$myUser" | awk '{print $NF}'
     
