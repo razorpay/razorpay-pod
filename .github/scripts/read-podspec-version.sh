1ab9a27ef6ea5bbff75e7e260870591cc623627a
Ramprasad A <ramprasad.a@razorpay.com>
2022-08-29 18:58:49 +0530

    Syntax fix 10

--- .github/scripts/read-podspec-version.sh
+++ .github/scripts/read-podspec-version.sh
@@ -4,7 +4,7 @@ file="razorpay-pod.podspec"     #the file where you keep your string name
 
 NEW_PODSPEC_VERSION=$1
 
-versionLine="$(grep 's.version ' $file | sed 's/"//g')"
+versionLine="$(grep 's.version ' $file | sed 's/"//g' -e 's/"$//')"
 version=${versionLine##* }
 
 echo $version
