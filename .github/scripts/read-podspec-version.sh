a0bffffcc97bce2d0d5f8aecf20456414eb38780
Ramprasad A <ramprasad.a@razorpay.com>
2022-08-29 18:34:33 +0530

    Remove quotes while read

--- .github/scripts/read-podspec-version.sh
+++ .github/scripts/read-podspec-version.sh
@@ -4,8 +4,10 @@ file="razorpay-pod.podspec"     #the file where you keep your string name
 
 NEW_PODSPEC_VERSION=$1
 
-versionLine=$(grep 's.version ' $file)
+versionLine="$(grep 's.version ' $file)"
 version=${versionLine##* }
+
+sed -e 's/^"//' -e 's/"$//' <<< $version
 
 echo $version
 
