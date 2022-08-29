c7b53e531a33f9fc1f9048d661ab57d235565325
Ramprasad A <ramprasad.a@razorpay.com>
2022-08-29 21:07:22 +0530

    Restored Podsepc version Read podspec version input removed

--- .github/scripts/read-podspec-version.sh
+++ .github/scripts/read-podspec-version.sh
@@ -2,8 +2,6 @@
 
 file="razorpay-pod.podspec"     #the file where you keep your string name
 
-NEW_PODSPEC_VERSION=$1
-
 versionLine="$(grep 's.version ' $file | sed 's/"//g' | sed "s/'//g")"
 version=${versionLine##* }
 
