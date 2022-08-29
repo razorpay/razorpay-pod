a0fc497bfca8095b730b431a2c0a7de3b36d6cff
Ramprasad A <ramprasad.a@razorpay.com>
2022-08-29 15:35:21 +0530

    Added release cocoapod workflow

---
+++ .github/scripts/read-podspec-version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+file="razorpay-pod.podspec"     #the file where you keep your string name
+
+NEW_PODSPEC_VERSION=$1
+
+versionLine="$(grep 's.version ' $file)"
+version=${versionLine##* }
+
+echo "$version"
+
+# echo "$myUser" | awk '{print $NF}'
+    
