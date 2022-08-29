3c0a076c3e6f285f5571b3ced60e4d79a7146e6b
Ramprasad A <ramprasad.a@razorpay.com>
2022-08-29 18:44:00 +0530

    Test fix 6

--- .github/scripts/read-podspec-version.sh
+++ .github/scripts/read-podspec-version.sh
@@ -7,9 +7,9 @@ NEW_PODSPEC_VERSION=$1
 versionLine="$(grep 's.version ' $file)"
 version=${versionLine##* }
 
-sed -e 's/^"//' -e 's/"$//' <<<$version
+echo version| sed -re 's/(\<.+)\x27(.+\>)/\1 \2/g'
 
-echo $version
+# echo $version
 
 # echo "$myUser" | awk '{print $NF}'
     
