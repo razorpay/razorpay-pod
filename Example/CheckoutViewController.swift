39e6161e69c4aa1591e4b8b0e75e2164b84d851b
Vivek Shindhe <vivek.shindhe@razorpay.com>
2024-01-31 13:30:08 +0530

    v1.3.

--- Example/CheckoutViewController.swift
+++ Example/CheckoutViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 import Razorpay
 import SafariServices
 
-class CheckoutViewController: UIViewController {
+class CheckoutViewController: UIViewController,RazorpayPaymentCompletionProtocolWithData {
     
     var razorpayObj : RazorpayCheckout? = nil
     var merchantDetails : MerchantsDetails = MerchantsDetails.getDefaultData()
     
-    let razorpayKey = "rzp_live_ILgsfZCZoFIKMb" // Sign up for a Razorpay Account(https://dashboard.razorpay.com/#/access/signin) and generate the API Keys(https://razorpay.com/docs/payment-gateway/dashboard-guide/settings/#api-keys/) from the Razorpay Dashboard.
+    let razorpayKey = "rzp_test_1DP5mmOlF5G5ag" // Sign up for a Razorpay Account(https://dashboard.razorpay.com/#/access/signin) and generate the API Keys(https://razorpay.com/docs/payment-gateway/dashboard-guide/settings/#api-keys/) from the Razorpay Dashboard.
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,10 +55,19 @@ class CheckoutViewController: UIViewController {
         controller.delegate = self
     }
     
+    private func presentAlert(withTitle title: String?, message : String?) {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "Okay", style: .default)
+            alertController.addAction(OKAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
     
     private func openRazorpayCheckout() {
         // 1. Initialize razorpay object with provided key. Also depending on your requirement you can assign delegate to self. It can be one of the protocol from RazorpayPaymentCompletionProtocolWithData, RazorpayPaymentCompletionProtocol.
-        razorpayObj = RazorpayCheckout.initWithKey(razorpayKey, andDelegate: self)
+        razorpayObj = RazorpayCheckout.initWithKey(razorpayKey, andDelegateWithData: self)
         let options: [AnyHashable:Any] = [
             "prefill": [
                 "contact": "1234567890",
@@ -78,6 +87,14 @@ class CheckoutViewController: UIViewController {
             print("Unable to initialize")
         }
     }
+    
+    func onPaymentSuccess(_ payment_id: String, andData response: [AnyHashable : Any]?) {
+        self.presentAlert(withTitle: "Payment Successful", message: response?.description)
+    }
+    
+    func onPaymentError(_ code: Int32, description str: String, andData response: [AnyHashable : Any]?) {
+        self.presentAlert(withTitle: "Payment Failed", message: str+"\n"+response!.description)
+    }
 }
 
 extension CheckoutViewController : CustomizeDataDelegate {
@@ -89,43 +106,43 @@ extension CheckoutViewController : CustomizeDataDelegate {
 }
 
 // RazorpayPaymentCompletionProtocol - This will execute two methods 1.Error and 2. Success case. On payment failure you will get a code and description. In payment success you will get the payment id.
-extension CheckoutViewController : RazorpayPaymentCompletionProtocol {
-    
-    func onPaymentError(_ code: Int32, description str: String) {
-        print("error: ", code, str)
-        self.presentAlert(withTitle: "Alert", message: str)
-    }
-    
-    func onPaymentSuccess(_ payment_id: String) {
-        print("success: ", payment_id)
-        self.presentAlert(withTitle: "Success", message: "Payment Succeeded")
-    }
-}
+//extension CheckoutViewController : RazorpayPaymentCompletionProtocol {
+//    
+//    func onPaymentError(_ code: Int32, description str: String) {
+//        print("error: ", code, str)
+//        self.presentAlert(withTitle: "Alert", message: str)
+//    }
+//    
+//    func onPaymentSuccess(_ payment_id: String) {
+//        print("success: ", payment_id)
+//        self.presentAlert(withTitle: "Success", message: "Payment Succeeded")
+//    }
+//}
 
 // RazorpayPaymentCompletionProtocolWithData - This will returns you the data in both error and success case. On payment failure you will get a code and description. In payment success you will get the payment id.
-extension CheckoutViewController: RazorpayPaymentCompletionProtocolWithData {
-    
-    func onPaymentError(_ code: Int32, description str: String, andData response: [AnyHashable : Any]?) {
-        print("error: ", code)
-        self.presentAlert(withTitle: "Alert", message: str)
-    }
-    
-    func onPaymentSuccess(_ payment_id: String, andData response: [AnyHashable : Any]?) {
-        print("success: ", payment_id)
-        self.presentAlert(withTitle: "Success", message: "Payment Succeeded")
-    }
-}
+//extension CheckoutViewController: RazorpayPaymentCompletionProtocolWithData {
+//    
+//    func onPaymentError(_ code: Int32, description str: String, andData response: [AnyHashable : Any]?) {
+//        print("error: ", code)
+//        self.presentAlert(withTitle: "Alert", message: str)
+//    }
+//    
+//    func onPaymentSuccess(_ payment_id: String, andData response: [AnyHashable : Any]?) {
+//        print("success: ", payment_id)
+//        self.presentAlert(withTitle: "Success", message: "Payment Succeeded")
+//    }
+//}
 
 
 extension CheckoutViewController {
-    func presentAlert(withTitle title: String?, message : String?) {
-        DispatchQueue.main.async {
-            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let OKAction = UIAlertAction(title: "Okay", style: .default)
-            alertController.addAction(OKAction)
-            self.present(alertController, animated: true, completion: nil)
-        }
-    }
+//    func presentAlert(withTitle title: String?, message : String?) {
+//        DispatchQueue.main.async {
+//            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//            let OKAction = UIAlertAction(title: "Okay", style: .default)
+//            alertController.addAction(OKAction)
+//            self.present(alertController, animated: true, completion: nil)
+//        }
+//    }
 }
 
 extension CheckoutViewController : SFSafariViewControllerDelegate {
