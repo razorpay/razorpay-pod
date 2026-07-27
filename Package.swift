3527160c689c29e792082d1c17cbae85d4d4c31a
gauravbhardwaj-7 <gaurav.bhardwaj@razorpay.com>
2026-07-27 15:32:25 +0530

    refactor(ci): changed the old deploy-checkout-yml to deploy.yml for SPM releases

--- Package.swift
+++ Package.swift
@@ -1,56 +1,53 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
-let packageVersion = "1.4.1"
-
 let package = Package(
     name: "RazorpayCheckout",
     platforms: [
         .iOS(.v13)
     ],
     products: [
-        // This product exposes everything needed to use Razorpay
-        .library(
-            name: "RazorpayCheckout",
-            targets: [
-                "RazorpayCheckout",
-                "Razorpay",
-                "RazorpayCore",
-                "RazorpayStandard"
-            ]
-        ),
+        .library(name: "RazorpayCheckout", targets: ["RazorpayCheckout", "Razorpay", "RazorpayCore", "RazorpayStandard"]),
+        .library(name: "RazorpayCustomUI",  targets: ["RazorpayCustomUI"]),
     ],
     targets: [
         .target(
             name: "RazorpayCheckout",
-            dependencies: [
-                .target(name: "Razorpay"),
-                .target(name: "RazorpayCore"),
-                .target(name: "RazorpayStandard"),
-            ],
+            dependencies: ["Razorpay", "RazorpayCore", "RazorpayStandard"],
             path: "RazorpayCheckout/Sources/RazorpayCheckoutCore"
+        ),
+        .target(
+            name: "RazorpayCustomUI",
+            dependencies: ["Razorpay", "RazorpayCore", "RazorpayCustom"],
+            path: "RazorpayCustomUI/Sources"
         ),
 
         .binaryTarget(
             name: "Razorpay",
-            path: "Pod/core/Razorpay.xcframework"
+            url: "https://github.com/razorpay/razorpay-pod/releases/download/REL_CORE/Razorpay.xcframework.zip",
+            checksum: "CKSUM_WRAPPER"
         ),
-
-        .binaryTarget(
-            name: "RazorpayStandard",
-            path: "Pod/RazorpayStandard.xcframework"
-        ),
-
         .binaryTarget(
             name: "RazorpayCore",
-            path: "Pod/core/RazorpayCore.xcframework"
+            url: "https://github.com/razorpay/razorpay-pod/releases/download/REL_CORE/RazorpayCore.xcframework.zip",
+            checksum: "CKSUM_CORE"
+        ),
+        .binaryTarget(
+            name: "RazorpayStandard",
+            url: "https://github.com/razorpay/razorpay-pod/releases/download/REL_STANDARD/RazorpayStandard.xcframework.zip",
+            checksum: "CKSUM_STANDARD"
+        ),
+        .binaryTarget(
+            name: "RazorpayCustom",
+            url: "https://github.com/razorpay/razorpay-pod/releases/download/REL_CUSTOM/RazorpayCustom.xcframework.zip",
+            checksum: "CKSUM_CUSTOM"
         ),
 
         .testTarget(
             name: "RazorpayCheckoutTests",
             dependencies: ["RazorpayCheckout"],
             path: "RazorpayCheckout/Tests/RazorpayCheckoutTests"
-        )
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
